e807f61b0860e9fa6bb4d35280a21e34b3ea533d
João Pegarare <jppegarare18@gmail.com>
2023-06-14 17:38:44 -0400

    adicionei try e catch que aprendemos nas ultimas aulas

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // divListaProduto.insertAdjacentHTML('afterbegin', '');
 
+class MeuErro extends Error {
+  constructor(message){
+    super(message);
+    this.name = "Erro de Preenchimento";
+  }
+}
+
 class Produto
 {
     constructor (nome, dCadastro, descricao, preco)
@@ -13,14 +20,32 @@ class Produto
 
     mostrarProduto()
     {
+      try{
+        return this.atributos()
+      }
+      catch(erro){
+        console.log(erro)
+      }
+    }
 
-      return `
-      <h3>${this.nome}</h1>
-      <p>${this.preco}</p>
-      <p>Data de cadastro: ${this.dCadastro}</p>
-      <p>${this.descricao}</p>
+    atributos()
+    {
+      if(this.nome != "" && this.dCadastro != "" && this.descricao != ""  && this.preco != "")
+      {
+        return `
+          <h2>${this.nome}</h2>  
+          <div>${this.descricao}</div>
+          <p>Data de cadastro: ${this.dCadastro}</p>
+          <p>${this.preco}</p>
+        `
+      }
+      else
+      {
+        throw new MeuErro("Insira todos os atributos que estão faltando")
+      }
 
-      `    }
+  }
+    
 }
 
 class ProdutoDestaque extends Produto
@@ -33,13 +58,26 @@ class ProdutoDestaque extends Produto
 
   mostrar_destaque()
   {
-    return `
-    <h1> ${this.nome}</h1>
-    <img src="${this.img}"></img>
-    <p>${this.preco}</p>
-    <p>Data de cadastro: ${this.dCadastro}</p>
-    <p>${this.descricao}</p>
-    `
+
+    try{
+      return this.atributos()
+    }
+    catch(erro){
+      console.log(erro)
+    }
+  }
+  atributos(){
+    if(this.nome != "" && this.dCadastro != "" && this.descricao != "" && this.preco != ""  && this.img != ""){
+      return `
+      <h1>${this.nome}</h1>
+      <img src="${this.img}"></img>
+      <p>${this.preco}</p>
+      <p>Data de cadastro: ${this.dCadastro}</p>
+      <p>${this.descricao}</p>
+      `}
+    else{
+      throw new MeuErro("Insira todos os atributos que estão faltndo")
+    }
   }
 
 }
@@ -51,11 +89,9 @@ let p3 = document.getElementById("produto3")
 
 const produtoD = new ProdutoDestaque ("PlayStation 5", "14/10/2020" , "Um console de nova geração com jogos até 120fps", "R$ 4.799,99", "https://i.promobit.com.br/268/456342844416003668966101124919.png" )
 
-const produto1 = new Produto ("Xbox Series S", "10/11/2020", "O console mais barato da nova geração!", "R$ 2.599,90")
-const produto2 = new Produto ("Fone de Ouvido Havit HV-H2002d", "18/11/2019", "Um dos melhores fones de ouvido custo beneficio!", "R$ 159,90")
-const produto3 = new Produto ("Livro 1984", "21/06/2012", "Um Livro de Ficção com uma crítica real por trás de sua história", "R$ 19,99")
-
-
+let produto1 = new Produto ("Xbox Series S", "10/11/2020", "O console mais barato da nova geração!", "R$ 2.599,90")
+let produto2 = new Produto ("Fone de Ouvido Havit HV-H2002d", "18/11/2019", "Um dos melhores fones de ouvido custo beneficio!", "R$ 159,90")
+let produto3 = new Produto ("Livro 1984", "21/06/2012", "Um Livro de Ficção com uma crítica real por trás de sua história", "R$ 19,99")
 
 
 
